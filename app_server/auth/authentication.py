ba423372559bd3964aea026a6749d12b18a04d63
Tom Victor <vjtomvictor@icloud.com>
2021-05-04 19:10:59 +0530

    Configured auth_server

---
+++ app_server/auth/authentication.py
@@ -0,0 +1,5 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class CustomJWTAuthentication(JWTAuthentication):
+    pass
---
+++ app_server/auth/backends.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.backends import ModelBackend
+
+
+class CustomModelBackend(ModelBackend):
+    pass
